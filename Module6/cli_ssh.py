7792ae8836b207b28a6e0373e6e70728aae2e88c
Sergei Garbuzov <gsf@elbrys.com>
2015-10-19 17:43:10 -0400

    add example of CLI commands execution over TELNET session

--- Module6/cli_ssh.py
+++ Module6/cli_ssh.py
@@ -11,13 +11,14 @@ import socket
 import paramiko
 
 
-# Remote device SSH session specific data that need change
+# SSH session specific data that need change
 IP_ADDR = '172.22.17.110'
 PORT_NUM = 830
 USERNAME = 'vyatta'
 PASSWORD = 'vyatta'
-# The maximum amount of data to be received from the remote
-# device during CLI session
+
+# CLI session specific
+TIMEOUT = 5
 RCV_BUFF_SIZE = 1000
 
 
@@ -47,10 +48,10 @@ def cli_get_interfaces(rsh):
     return output
 
 
-def connect_ssh(ip, port, uname, pswd):
+def connect_ssh(ip, port=22, uname, pswd):
     '''Establish SSH connection to a remote device'''
 
-    # Create an instance of SSHClient object
+    # Create an instance object of the 'SSHClient' class
     rconn = paramiko.SSHClient()
 
     # Allow auto adding of unknown hosts to the known hosts in local
@@ -58,13 +59,13 @@ def connect_ssh(ip, port, uname, pswd):
     # policy requirements)
     rconn.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-    # establish SSH connection
-    # o 'look_for_keys' is set to False to disable searching
-    #   for discoverable private key files in '~/.ssh/'
-    # o 'allow_agent' is set to False to disable connecting
-    #   to the SSH agent on the local machine for obtaining
-    #   the private key
+    # Establish SSH connection
     try:
+        # o 'look_for_keys' is set to False to disable searching
+        #   for discoverable private key files in '~/.ssh/'
+        # o 'allow_agent' is set to False to disable connecting
+        #   to the SSH agent on the local machine for obtaining
+        #   the private key
         rconn.connect(ip, username=uname, password=pswd, port=830,
                       look_for_keys=False, allow_agent=False)
         return rconn
@@ -77,39 +78,37 @@ def connect_ssh(ip, port, uname, pswd):
 
 
 def disconnect_ssh(rconn):
-    '''Terminate SSH connection with a remote device'''
+    '''Close SSH connection to a remote device'''
     try:
         rconn.close()
     except (Exception) as e:
         print "!!!Error, %s " % e
 
 
-if __name__ == '__main__':
+# Connect to a remote SSH server
+ssh_conn = connect_ssh(ip=IP_ADDR, port=PORT_NUM,
+                       uname=USERNAME, pswd=PASSWORD)
+if ssh_conn:
+    print "Established SSH connection to %s\n" % IP_ADDR
+    try:
+        # Start an interactive shell session on the remote SSH server
+        rsh = ssh_conn.invoke_shell()
+        print "Initiated interactive shell session\n"
 
-    # Connect to a remote SSH server
-    ssh_conn = connect_ssh(ip=IP_ADDR, port=PORT_NUM,
-                           uname=USERNAME, pswd=PASSWORD)
-    if ssh_conn:
-        print "Established SSH connection to %s\n" % IP_ADDR
-        try:
-            # Start an interactive shell session on the remote SSH server
-            rsh = ssh_conn.invoke_shell()
-            print "Initiated interactive shell session\n"
+        # Read the initial prompt data from the channel and display it
+        output = rsh.recv(RCV_BUFF_SIZE)
+        print "%s\n" % output
 
-            # Read the initial prompt data from the channel and display it
-            output = rsh.recv(RCV_BUFF_SIZE)
-            print "%s\n" % output
+        # Turn off CLI paging
+        cli_disable_paging(rsh)
 
-            # Turn off CLI paging
-            cli_disable_paging(rsh)
-
-            # Get list of interfaces available on the device and display it
-            output = cli_get_interfaces(rsh)
-            print "%s\n" % output
-        except (paramiko.SSHException) as e:
-            print "!!!Error: %s\n" % e
-        finally:
-            disconnect_ssh(ssh_conn)
-            print "Closed SSH connection to %s\n" % IP_ADDR
-    else:
-        print "SSH connection to %s has failed\n" % IP_ADDR
+        # Get list of interfaces available on the device and display it
+        output = cli_get_interfaces(rsh)
+        print "%s\n" % output
+    except (paramiko.SSHException) as e:
+        print "!!!Error: %s\n" % e
+    finally:
+        disconnect_ssh(ssh_conn)
+        print "Closed SSH connection to %s\n" % IP_ADDR
+else:
+    print "SSH connection to %s has failed\n" % IP_ADDR
---
+++ Module6/cli_telnet.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python
+
+# Example of a script that executes CLI commands on a remote
+# device via TELNET connection
+
+# built-in modules
+import telnetlib
+
+# TELNET session specific data
+IP_ADDR = '172.22.17.110'
+PORT_NUM = 23
+USERNAME = 'vyatta'
+PASSWORD = 'vyatta'
+
+# CLI session specific data
+TIMEOUT = 5
+OPER_PROMPT = '$'
+ADMIN_PROMPT = '#'
+
+
+def cli_disable_paging(rsh):
+    """Execute CLI command that disables CLI paging on a remote device"""
+
+    # Execute the command
+    cmd = 'set terminal length 0\n'
+    rsh.write(cmd)
+
+    # Read device output until one from a list of regular expressions
+    # matches or until timeout
+    r = rsh.expect(['Invalid command', '\%s' % OPER_PROMPT], timeout=TIMEOUT)
+    if r[0] != 1:
+        print "!!!Failed to execute CLI command: %s" % cmd
+        # Flash out the read buffer
+        rsh.read_until(match=OPER_PROMPT, timeout=TIMEOUT)
+        return False
+    else:
+        return True
+
+
+def cli_enter_cfg_mode(rsh):
+    """Execute CLI command for entering the configuration mode
+    on a remote device
+    """
+
+    # Execute the command
+    cmd = "configure\n"
+    rsh.write(cmd)
+
+    # Read device output until one from a list of regular expressions
+    # matches or until timeout
+    r = rsh.expect(['Invalid command', ADMIN_PROMPT], timeout=TIMEOUT)
+    if r[0] != 1:
+        print "!!!Failed to execute CLI command: %s" % cmd
+        # Flash out the read buffer
+        rsh.read_until(OPER_PROMPT, TIMEOUT)
+        return False
+    else:
+        return True
+
+
+def cli_get_interfaces(rsh):
+    """Execute CLI command that shows interface information
+    on a remote device
+    """
+
+    # Execute the command
+    cmd = 'show interfaces\n'
+    rsh.write(cmd)
+
+    # Read device output until one from a list of regular expressions
+    # matches or until timeout
+    output = None
+    r = rsh.expect([r'Invalid command', r'#'], timeout=TIMEOUT)
+    if r[0] != 1:
+        print "!!!Failed to execute CLI command: %s" % cmd
+        # Flash out the read buffer
+        rsh.read_until(ADMIN_PROMPT, timeout=TIMEOUT)
+    else:
+        output = r[2]
+
+    return output
+
+
+def connect_telnet(ip, port=23, uname=None, pswd=None, timeout=None):
+    """
+    Establish TELNET connection to a remote device.
+
+    Returns:
+        On success, instance object that represents connection
+        to a remote TELNET server. None on failure.
+    """
+
+    # Create an instance object of the 'Telnet' class
+    rconn = telnetlib.Telnet()
+
+    # Establish TELNET connection
+    try:
+        # Uncomment the following line to enable 'telnetlib' debug tracing
+        # rconn.set_debuglevel(1)
+
+        # Connect to device
+        rconn.open(ip, port, timeout)
+        # Login to device
+        if uname:
+                rconn.read_until('login:')
+                rconn.write('%s\n' % uname)
+        if pswd:
+            rconn.read_until('assword:')
+            rconn.write('%s\n' % pswd)
+
+        # Read device output until one from a list of regular expressions
+        # matches or until timeout
+        r = rconn.expect(['Login incorrect', '\%s' % OPER_PROMPT], timeout)
+        if r[0] == -1:
+            return None
+        else:
+            return rconn
+    except (Exception) as e:
+        print "!!!Error: %s " % e
+        return None
+
+
+def disconnect_telnet(rconn):
+    """Close TELNET connection to a remote device"""
+    try:
+        rconn.close()
+    except (Exception) as e:
+        print "!!!Error, %s " % e
+
+
+# Connect to a remote TELNET server
+rsh = connect_telnet(ip=IP_ADDR, port=PORT_NUM,
+                     uname=USERNAME, pswd=PASSWORD, timeout=TIMEOUT)
+if rsh:
+    print "Established TELNET connection to %s\n" % IP_ADDR
+
+    # Turn off CLI paging
+    cli_disable_paging(rsh)
+
+    # Enter configuration mode
+    cli_enter_cfg_mode(rsh)
+
+    # Read and show list of interfaces
+    output = cli_get_interfaces(rsh)
+    print "%s\n" % output
+
+    # Terminate TELNET session
+    disconnect_telnet(rsh)
+    print "Closed TELNET connection to %s\n" % IP_ADDR
+else:
+    print "TELNET connection to %s has failed\n" % IP_ADDR
