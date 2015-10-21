322d1c7f32c731e27a1df91003b5ebedaafeaa65
Sergei Garbuzov <gsf@elbrys.com>
2015-10-21 13:50:13 -0400

    provide an example of anonymous function (lambda expression)

---
+++ Module6/lambda_expression.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+
+# Examples of using lambda expression
+#
+# NOTES:
+# Like def, lambda expression creates a function object to be called later,
+# but it returns the function object instead of assigning it to a name.
+# Since lambda is an expression (not a statement) it can appear in places
+# where a def is not allowed by Python's syntax, for example inside a list
+# literal.
+# The lambda's body is a single expression, not a block of statements, it is
+# designed for coding simple constructs with a small bits of executable code,
+# and def handles larger tasks.
+# Lambdas are entirely optional, defs can always be used instead.
+
+
+device1 = {'vendor': 'Cisco', 'os_type': 'IOS'}
+device2 = {'vendor': 'Cisco', 'os_type': 'IOS-XR'}
+device3 = {'vendor': 'Cisco', 'os_type': 'NX-OS'}
+device4 = {'vendor': 'HP', 'os_type': 'Comware'}
+device5 = {'vendor': 'HP', 'os_type': 'Procurve'}
+device6 = {'vendor': 'Brocade', 'os_type': 'VDX'}
+device7 = {'vendor': 'Brocade', 'os_type': 'ICX'}
+device8 = {'vendor': 'Brocade', 'os_type': 'MLX'}
+
+all_devices = [device1, device2, device3, device4,
+               device5, device6, device7, device8]
+print "\n"
+print "List of all network devices:"
+for device in all_devices:
+    print " %s" % device
+
+#
+# Functionality implemented via use of lambda expression
+#
+
+print "\n"
+print "%s" % ("-" * 70)
+print " Device selection implemented with the use of lambda expression"
+print "%s" % ("-" * 70)
+cisco_devices = filter(lambda x: x['vendor'].lower() == 'cisco'.lower(),
+                       all_devices)
+print "\n"
+print "List of Cisco network devices:"
+for device in cisco_devices:
+    print " %s" % device
+
+hp_devices = filter(lambda x: x['vendor'].lower() == 'hp'.lower(),
+                    all_devices)
+print "\n"
+print "List of HP network devices: %s"
+for device in hp_devices:
+    print " %s" % device
+
+brocade_devices = filter(lambda x: x['vendor'].lower() == 'brocade',
+                         all_devices)
+print "\n"
+print "List of Brocade network devices:"
+for device in brocade_devices:
+    print " %s" % device
+
+
+#
+# Similar functionality implemented with the use of def function
+#
+
+print "\n"
+print "%s" % ("-" * 70)
+print " Device selection implemented with the use of def function"
+print "%s" % ("-" * 70)
+
+
+def get_vendor_devices(dev_list, vendor):
+    vlist = []
+    for item in dev_list:
+        if item['vendor'].lower() == vendor.lower():
+            vlist.append(item)
+    return vlist
+
+cisco_devices = get_vendor_devices(all_devices, 'cisco')
+print "\n"
+print "List of Cisco network devices:"
+for device in cisco_devices:
+    print " %s" % device
+
+hp_devices = get_vendor_devices(all_devices, 'hp')
+print "\n"
+print "List of HP network devices: %s"
+for device in hp_devices:
+    print " %s" % device
+
+
+brocade_devices = get_vendor_devices(all_devices, 'brocade')
+print "\n"
+print "List of Brocade network devices:"
+for device in brocade_devices:
+    print " %s" % device
+
+print "\n"
