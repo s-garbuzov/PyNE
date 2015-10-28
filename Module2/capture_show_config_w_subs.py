b64504f118d7e058f71e7a7abcaf788e05157f98
igolem <freegolem@gmail.com>
2015-10-28 00:01:48 -0500

    Update capture_show_config_w_subs.py

--- Module2/capture_show_config_w_subs.py
+++ Module2/capture_show_config_w_subs.py
@@ -3,72 +3,81 @@
 # description:
 # this script does the following:
 #     logs into a single switch
-#     copies the config to a file
 #     captures the output of the running config
 #     writes the output of the running config to a file
 
 
-import getpass, pexpect
+import getpass, pexpect, sys
 
-# assign variables
-switch = '10.3.21.5'
-user = raw_input('Enter username: ')
-pwd = getpass.getpass(prompt='Enter password for ' + user + ': ')
-enable_pwd = pwd # set the enable password to the same value as password for the lab exercis
-prompt = '#'
-pexpect_timeout = 30 # set timeout for pexpect (i.e., how long to wait for match conditions)
-switch_config = [] # create global list to hold config
 
-# open file to write configuration to
-config_output_filename = 'config_output.txt'
-config_output = open(config_output_filename, 'w')
+def get_user_credentials():
+	# get user input
+	user = raw_input('Enter username: ')
+	pwd = getpass.getpass(prompt='Enter password for ' + user + ': ')
+	return (user, pwd)
 
-def setup_session(switch):
-	print 'Logging into and setting session parameters for', switch
-	telnet_session.sendline(user)
-	telnet_session.expect('Password:')
-	telnet_session.sendline(pwd)
-	telnet_session.expect('>')
-	telnet_session.sendline('enable')
-	telnet_session.expect('Password:')
-	telnet_session.sendline(enable_pwd)
-	telnet_session.expect(prompt)
+
+def establish_telnet_session(switch, switch_prompt, username, password):
+	pexpect_timeout = 30 # set timeout for pexpect (i.e., how long to wait for match conditions)
+	enable_pwd = pwd # set the enable password to the same value as password for this lab exercise
+
+	# telnet to switch, get config, and write config to file
+	telnet_cmd = 'telnet ' + switch
+	telnet_session = pexpect.spawn(telnet_cmd, timeout=pexpect_timeout)
+	result = telnet_session.expect(['Username:', pexpect.EOF, pexpect.TIMEOUT])
+
+	# proceed given the condition you are presented with a username prompt
+	if result==0:
+		telnet_session.sendline(user)
+		telnet_session.expect('Password:')
+		telnet_session.sendline(pwd)
+		telnet_session.expect('>')
+		telnet_session.sendline('enable')
+		telnet_session.expect('Password:')
+		telnet_session.sendline(enable_pwd)
+		telnet_session.expect(prompt)
+		return telnet_session
+
+	# an EOF will indicate the telnet executable exited
+	elif result==1:
+		sys.exit('Telnet exited unexpectedly while trying to connect to', switch) 
+
+	# pexpect timed out waiting for a match condition
+	elif result==2:
+		sys.exit('Pexpect timed out waiting for a username prompt for', switch) 
+
+
+def setup_session(telnet_session, switch_prompt):
 	telnet_session.sendline('terminal length 0')
 	telnet_session.expect(prompt)
 
-def show_config ():
-	print 'Getting running configuration for ', switch
+
+def show_config (switch):
 	telnet_session.sendline('show running-config')
 	telnet_session.expect(prompt)
 	show_run_output = telnet_session.before
+	return show_run_output
 
-	# set the switch_config list to contain the output of "show run", by converting into list elements by newlines in the raw show_run_output info
-	switch_config = show_run_output.splitlines()
 
-def write_config ():
-	print 'Writing config to file named', config_output_filename, 'for', switch
-	for line in switch_config:
-		print >> config_output, line
+def write_config (config_filename, config):
+	config_file = open(config_filename, 'w')
+	print >> config_file, config
+	config_output.close()
 
-# telnet to switch, get config, and write config to file
-telnet_cmd = 'telnet ' + switch
-telnet_session = pexpect.spawn(telnet_cmd, timeout=pexpect_timeout)
-result = telnet_session.expect(['Username:', pexpect.EOF, pexpect.TIMEOUT])
 
-# proceed given the condition you are immediately presented with a password prompt
-elif result==0:
-	setup_session()
-	show_config(switch)
-	write_config(switch,config_output_filename)
+def main():
+	# declare variables
+	switch = '10.3.21.5'
+	switch_prompt = '#'
+	config_filename = 'config_output.txt'
+
+	(username, password) = get_user_credentials()
+	telnet_session = establish_telnet_session(switch, switch_prompt, username, password)
+	setup_session(telnet_session, switch_prompt)
+	config = show_config(switch)
+	write_config(config_filename, config)
 	telnet_session.kill(0)
 
-# an EOF will indicate the telnet executable exited
-elif result==1:
-	print 'Telnet exited unexpectedly while trying to connect to', switch
 
-# pexpect timed out waiting for a match condition
-elif result==2:
-	print 'Pexpect timed out waiting for a match condition while connecting to', switch
-
-# close the config file
-config_output.close()
+if __name__ == '__main__':
+	main ()
