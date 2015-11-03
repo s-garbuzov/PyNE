e6ddb8dc246a3c7f3f4314175aba209ed14f87ea
igolem <freegolem@gmail.com>
2015-11-02 20:05:29 -0600

    Update get-credentials-via-file.py

--- Module2/get-credentials-via-file.py
+++ Module2/get-credentials-via-file.py
@@ -3,18 +3,35 @@
 # description: getting credentials from a file
 
 # open the credentials file in read-only mode
-# note: if you use a file to store passwords, you will need to do the following in production
-#     keep usernames/password credentials in a different directory than the script
-#     use file permissions to restrict access to the information
-#     run the script as a different user (or possibly sudo) that has access to the credentials
-credential_file = open('./credentials.txt', 'r')
+# note: if you use a file to store passwords,
+# you will need to do the following in production
+#    1. keep the credential file in a different directory than the script
+#    2. use file permissions to restrict access to credentials
+#    3. run the script as a user with privileges to the credential file
 
-for line in credential_file:
-	line_parts = line.split(':')
-	if (line_parts[0] == 'username'):
-		username = line_parts[1].rstrip()
-	elif (line_parts[0] == 'password'):
-		password = line_parts[1].rstrip()
 
-print '\nThe password for', username, 'is', password
-print '\nPrinting passwords is a bad idea, but is done here for demonstration purposes.\n'
+def read_credential_file():
+    credential_file_name = './credentials.txt'
+    credential_file_object = open(credential_file_name, 'r')
+    
+    # "parse" the credential file to find credentials
+    # iterate over each line in the credential file
+    for line in credential_file_object:
+        # split the contents of the line into components
+        # create individual components separated by colons
+        line_components = line.split(':')
+        if (line_components[0] == 'username'):
+            username = line_components[1].rstrip()
+        elif (line_components[0] == 'password'):
+            password = line_components[1].rstrip()
+    
+    return (username, password)
+
+
+def main():
+    (username, password) = read_credential_file()
+    print '\nThe password for', username, 'is', password
+
+
+if __name__ == '__main__':
+    main ()
