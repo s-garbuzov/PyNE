5b220bc7b3da9e915a036090aec19206334af345
igolem <freegolem@gmail.com>
2015-11-02 20:08:24 -0600

    Update read-switch-list-from-file.py

--- Module2/read-switch-list-from-file.py
+++ Module2/read-switch-list-from-file.py
@@ -1,36 +1,46 @@
 #!/usr/bin/python
+# description: this script demonstrates reading a list of switches from a file
 
-# import the regular expression module
+
+# import regular expression module
 import re
 
-# define empty list to stuff switch names into
-# it is not strictly necessary to do this, but declaring variables at the beginning of a script is helpful
-#     as it gives you a quick reference point when the list of global variables is long
-switch_list = []
 
-# regular expression describing switch naming convention used to determine if a line in the file represents a switch name
-# note: the "^" is implied, so not strictly necessary
-#     and the \n ensures that the switch name is followed by a newline, and thus on a line by itself
-switch_name_format = re.compile (r'^[a-z0-9]+\-[0-9]+\.switch\.virl-lab\.cisco\.com\n')
+def get_switch_list():
+    # instantiate a list to append switch names to
+    switch_list = []
+    # define regular expression to match on a switch name
+    switch_name_format = \
+            re.compile(r'^[a-z0-9]+\-[0-9]+\.switch\.cml-lab\.cisco\.com\n')
 
-# create file object pointing to the file containing our list of switches and set to read only
-switch_file_object = open('./switch-file.txt', 'r')
+    switch_list_file_name = './switch-file.txt'
+    switch_list_file_object = open(switch_list_file_name, 'r')
 
-# iterate through each line read into the file object
-for line in switch_file_object:
-	
-	#check to see if the current line of input matches the regular expression defined in switch_name_format	
-	if switch_name_format.match(line):
+    # iterate through each line in the switch list file
+    for line in switch_list_file_object:
+        # if the line matches regex format, then name append to switch_list
+        if switch_name_format.match(line):
+            # remove white space on the right (including newlines)
+            line = line.rstrip()
+            switch_list.append(line)
 
-		# rstrip() removes all white space at the end of a line, including carriage returns
-		line = line.rstrip()
+    # close the file object
+    switch_list_file_object.close()
+    # return the contents of the switch_list variable to main()
+    return switch_list
 
-		# add the current line, which has matched the switch name regular expression, to the list of switches in switch_list
-		switch_list.append(line)
 
-# close the file object
-switch_file_object.close()
+def show_switch_list(switch_list):
+    # iterate over each list element in the switch_list variable
+    # and print it out
+    for switch_name in switch_list:
+        print switch_name
 
-# print the list of switches that were added to the switch_list variable to verify the script worked
-for switch_name in switch_list:
-	print switch_name
+
+def main():
+    switch_list = get_switch_list()
+    show_switch_list(switch_list)
+
+
+if __name__ == '__main__':
+    main ()
