9fd938ab5e456d2e4498973ea8f8636384b582d2
Sergei Garbuzov <gsf@elbrys.com>
2015-11-04 13:50:06 -0500

    cleanup 'command_telnet' script in module6

--- Module6/command_telnet.py
+++ Module6/command_telnet.py
@@ -14,6 +14,7 @@ command_telnet.py
 """
 
 # built-in modules
+import time
 import telnetlib
 
 
@@ -22,50 +23,61 @@ def disable_cli_paging(device, telnet_conn, read_delay=1):
     Disable CLI paging on a remote device.
     :param telnetlib.Telnet telnet_conn: an instance of TELNET client
         connected to a remote server.
-    :param int timeout: TELNET session timeout
+    :param int read_delay: time to wait for the CLI command to complete.
     :return: True on success, False otherwise
     """
 
     # Execute the command
-#    cmd = 'set terminal length 0\n'
     cmd = 'terminal length 0\n'
     telnet_conn.write(cmd)
-
-    # Read device output until one from a list of regular expressions
-    # matches or until timeout
-    prompt = device['oper_prompt']
-    r = telnet_conn.expect(['Invalid command', '\%s' % prompt],
-                           read_delay)
-    if r[0] != 1:
-        # Flush out the read buffer
-        telnet_conn.read_until(prompt, read_delay)
-        return False
-    else:
+    oper_prompt = "%r" % device['oper_prompt']
+    config_prompt = device['config_prompt']
+    dummy, match, dummy = telnet_conn.expect([oper_prompt, config_prompt],
+                                             read_delay)
+    if(match is not None):
         return True
+    else:
+        return False
+
+
+def check_config_mode(device, telnet_conn):
+    """
+    Check if CLI on the device is in configuration mode.
+    :param dict device: dictionary containing information about target device.
+    :param telnetlib.Telnet telnet_conn: an instance of TELNET client
+        connected to the device.
+    :return: True if CLI is in configuration mode, False otherwise
+    Returns a boolean
+    """
+    cmd = '\n'
+    telnet_conn.write(cmd)
+    config_prompt = device['config_prompt']
+    idx, dummy, dummy = telnet_conn.expect([config_prompt])
+    if(idx == 0):
+        return True
+    else:
+        return False
 
 
 def enter_cli_cfg_mode(device, telnet_conn, read_delay=1):
     """
     Enter CLI configuration mode on a remote device.
+    :param dict device: dictionary containing information about target device.
     :param telnetlib.Telnet telnet_conn: an instance of TELNET client
-        connected to a remote server.
-    :param int timeout: TELNET session timeout
+        connected to the device.
+    :param int read_delay: time to wait for command completion
     :return: True on success, False otherwise
     """
 
-    # Execute the command
+    if(check_config_mode(device, telnet_conn) is True):
+        return True
+
     cmd = "configure\n"
     telnet_conn.write(cmd)
-
-    # Read device output until one from a list of regular expressions
-    # matches or until timeout
-    admin_prompt = device['admin_prompt']
-    oper_prompt = device['oper_prompt']
-    r = telnet_conn.expect(['Invalid command', admin_prompt], read_delay)
-    if r[0] != 1:
-        # Flush out the read buffer
-        telnet_conn.read_until(oper_prompt, read_delay)
-        return False
+    config_prompt = device['config_prompt']
+    idx, dummy, dummy = telnet_conn.expect([config_prompt], read_delay)
+    if(idx == 0):
+        return True
     else:
         return True
 
@@ -73,9 +85,7 @@ def enter_cli_cfg_mode(device, telnet_conn, read_delay=1):
 def connect_telnet(device):
     """
     Establish TELNET connection to a remote device.
-
-    :param dict device: dictionary containing information for establishing
-        TELNET session to a target device.
+    :param dict device: dictionary containing information about target device.
     :return: an instance of telnetlib.Telnet class connected
         to remote TELNET server on success, None otherwise.
     """
@@ -91,44 +101,47 @@ def connect_telnet(device):
         ip_addr = device['ip_addr']
         port = device['port']
         timeout = device['timeout']
+        if(device['verbose']):
+            print("Connecting to %s:%s" % (device['ip_addr'], device['port']))
         telnet_client.open(ip_addr, port, timeout)
         if(device['verbose']):
-            print("TELNET connection to %s:%s has been established" %
+            print("Connection to %s:%s has been established" %
                   (device['ip_addr'], device['port']))
 
         # Login to device
         uname = device['username']
         if uname:
-            # login_prompt = 'login:'
             login_prompt = device['login_prompt']
             response = telnet_client.read_until(login_prompt, timeout)
             if login_prompt not in response:
-                if(device['verbose']):
-                    print("failed to get '%s' prompt" % login_prompt)
                 disconnect_telnet(device, telnet_client)
+                err_msg = ("Failed to get expected '%s' login prompt" %
+                           login_prompt)
+                raise ValueError(err_msg)
                 return None
             telnet_client.write('%s\n' % uname)
 
         pswd = device['password']
         if pswd:
-            # pswd_prompt = 'assword:'
             pswd_prompt = device['password_prompt']
             response = telnet_client.read_until(pswd_prompt, timeout)
             if pswd_prompt not in response:
-                if(device['verbose']):
-                    print("failed to get '%s' prompt" % pswd_prompt)
                 disconnect_telnet(device, telnet_client)
+                err_msg = ("Failed to get expected '%s' password prompt" %
+                           pswd_prompt)
+                raise ValueError(err_msg)
                 return None
             telnet_client.write('%s\n' % pswd)
 
-        # Check if we got to initial operator prompt
-#        oper_prompt = '\%s' % OPER_PROMPT
-        admin_prompt = device['admin_prompt']
-        r = telnet_client.expect([admin_prompt], timeout)
-        if(r[0] == -1):
-            if(device['verbose']):
-                print("failed to get '%s' prompt" % admin_prompt)
+        # Check if we got to the console prompt
+        oper_prompt = "%r" % device['oper_prompt']
+        config_prompt = device['config_prompt']
+        dummy, match, dummy = \
+            telnet_client.expect([oper_prompt, config_prompt], timeout)
+        if(match is None):
             disconnect_telnet(device, telnet_client)
+            err_msg = ("Login failed (uname=%s, pswd=%s)" % (uname, pswd))
+            raise ValueError(err_msg)
             return None
 
         if(device['verbose']):
@@ -143,20 +156,23 @@ def connect_telnet(device):
 
 
 def disconnect_telnet(device, telnet_client):
-    """Close TELNET connection to a remote device"""
+    """Close TELNET connection to a remote device
+    :param dict device: dictionary containing information about target device.
+    :param telnetlib.Telnet telnet_conn: an instance of TELNET client
+        connected to the device.
+    """
     try:
         telnet_client.close()
         if(device['verbose']):
-            print("TELNET connection to %s:%s has been closed" %
+            print("Connection to %s:%s has been closed" %
                   (device['ip_addr'], device['port']))
     except (Exception) as e:
-        print "!!!Error, %s " % e
+        print "!!!Error: %s " % e
 
 
 def execute_command(device, cli_command, read_delay=1):
     """Execute a CLI command on a remote device over established TELNET channel.
-    :param dict device: dictionary containing information for establishing
-                        TELNET session to a target device.
+    :param dict device: dictionary containing information about target device.
     :param str cli_command: CLI command to be executed.
     :param int read_delay: time to wait for the CLI command to complete.
     :return: output of the command on success, error message otherwise.
@@ -164,42 +180,35 @@ def execute_command(device, cli_command, read_delay=1):
 
     assert(isinstance(device, dict))
     output = None
-    verbose = device['verbose']
 
     # Connect to device and execute the command
     telnet_conn = connect_telnet(device)
-    if telnet_conn:
+    if(telnet_conn is not None):
         # Turn off CLI paging and enter configuration mode
         disable_cli_paging(device, telnet_conn, read_delay)
-#        enter_cli_cfg_mode(device, telnet_conn, read_delay)
+        enter_cli_cfg_mode(device, telnet_conn, read_delay)
 
-        # Execute and wait for command completion,
-        # then read result from the receive buffer
+        # Execute command and wait for command completion
         telnet_conn.write(cli_command)
+        if((device['verbose'])):
+            print("CLI command %r has been executed" % cli_command)
+        time.sleep(read_delay)
 
-        # Returns a tuple of three items: the index in the list of the
-        # first regular expression that matches; the match object
-        # returned; and the text read up till and including the match.
-        oper_prompt = "\%s" % device['oper_prompt']
-        admin_prompt = device['admin_prompt']
+        # Read result from the receive buffer
+        # 'expect' call returns a tuple of three items:
+        # - the index in the list of the first regular expression
+        #   that matches
+        # - the match object returned
+        # - the text read up till and including the match
+        oper_prompt = "%r" % device['oper_prompt']
+        config_prompt = device['config_prompt']
         dummy, match, text = \
-            telnet_conn.expect(['Invalid command', oper_prompt, admin_prompt],
-                               read_delay)
-        print "%s/%s/%s" % (dummy, match, text)
-        if match is None:
-            output = "Failed to execute command"
-        else:
+            telnet_conn.expect([oper_prompt, config_prompt], read_delay)
+        if match is not None:
             output = text
-            if((device['verbose'])):
-                print("CLI command %r has been executed" % cli_command)
 
-        # Terminate TELNET session
+        # Close connection
         disconnect_telnet(device, telnet_conn)
-    else:
-        output = "Failed to execute command"
-        if(verbose):
-            print("TELNET connection to %s:%s has failed\n" %
-                  (device['ip_addr'], device['port']))
 
     return output
 
@@ -215,7 +224,7 @@ def main():
         'login_prompt': 'sername:',
         'password_prompt': 'assword:',
         'oper_prompt': '$',
-        'admin_prompt': '#',
+        'config_prompt': '#',
         'secret': 'secret',
         'verbose': True
     }
@@ -223,11 +232,14 @@ def main():
     cmd_string = "show interfaces\n"
     print("\nCommand to be executed: %s" % cmd_string)
     output = execute_command(device, cmd_string, read_delay=1)
-    print("\nCommand execution result:\n")
-    print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
-    print output
-    print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-    print("\n")
+    if(output is not None):
+        print("\nCommand execution result:\n")
+        print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+        print output
+        print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+        print("\n")
+    else:
+        print("Failed to execute command")
 
 
 if __name__ == '__main__':
