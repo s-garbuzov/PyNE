3049507e527185b1529815bb125ef6e6ac066635
igolem <freegolem@gmail.com>
2015-11-12 21:39:24 -0600

    Create split.py

---
+++ Module2/split.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+# purpose:
+# split provided input using the provided pattern
+# and print out something useful for reference
+
+
+def main():
+    input_parts = []
+
+    print 'Script splits input using provided pattern and prints result.\n'
+    input = raw_input('Text: ')
+    pattern = raw_input('Regular Expression Pattern: ')
+    list = input.split(pattern)
+
+    print '\nOriginal text:\n' + input
+    print '\nResulting list:'
+    print list
+
+    print '\nList positions:\n'
+    position = 0
+    for part in list:
+        print str(position) + ': ' + list[position]
+        position += 1
+
+
+if __name__ == '__main__':
+    main ()
