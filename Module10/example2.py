921fb68286d974760bf7d854613eb03eba4516fb
Sergei Garbuzov <gsf@elbrys.com>
2015-11-30 18:51:50 -0500

    changes in module10 (final version)

--- Module10/example2.py
+++ Module10/example2.py
@@ -14,17 +14,35 @@ PEP 249.
 import os
 import sqlite3 as db
 
+# third-party modules
+import json
 
-def print_device_info(device_info):
+
+class Device(object):
+    """Class representing a network device."""
+
+    def __init__(self, name=None, os_type=None, ip_addr=None, iflist=None):
+        """The constructor of the class instance."""
+        self.name = name
+        self.os_type = os_type
+        self.ip_addr = ip_addr
+        self.iflist = iflist
+
+    def toJSON(self):
+        """Converts instance object to JSON."""
+        return json.dumps(self, default=lambda o: o.__dict__)
+
+
+def print_device_info(device):
     """Prints information about given object instance of the Device class."""
-    assert(isinstance(device_info, tuple))
-    print(" Device Name : %s" % device_info[0])
-    print(" OS Type     : %s" % device_info[1])
-    print(" IP Address  : %s" % device_info[2])
-    print(" Interfaces  : %s" % device_info[3])
+    assert(isinstance(device, Device))
+    print(" Device Name : %s" % device.name)
+    print(" OS Type     : %s" % device.os_type)
+    print(" IP Address  : %s" % device.ip_addr)
+    print(" Interfaces  : %s" % ", ".join(device.iflist))
 
 
-def db_create_device_table(db_path):
+def db_create_devices_table(db_path):
     """In the 'db_path' SQLite database file create the 'Devices' table if it
     does not exist yet.
     """
@@ -43,22 +61,19 @@ def db_create_device_table(db_path):
             # CREATE TABLE is the keyword telling the database system
             # to create a new table. The unique name or identifier for
             # the table follows the CREATE TABLE statement.
-            # The 'name', 'os_type', 'ip_address' and 'interfaces' are the
-            # column names for each row to be created in the table.
-            # PRIMARY KEY constraint uniquely identifies each row/record
-            # in a database table, primary key column cannot have NULL values.
-            table_name = "Devices"
-            sql = ("CREATE TABLE IF NOT EXISTS {tn}"
+            # The 'name' and 'info' are the column names for each row to be
+            # created in the table. PRIMARY KEY constraint uniquely identifies
+            # each row/record in a database table, primary key column cannot
+            # have NULL values.
+            sql = ("CREATE TABLE IF NOT EXISTS Devices"
                    "(name TEXT PRIMARY KEY NOT NULL,"
-                   "os_type TEXT,"
-                   "ip_address TEXT,"
-                   "interfaces TEXT)").format(tn=table_name)
+                   "info TEXT)")
             cursor.execute(sql)
     except (db.OperationalError) as e:
         print("!!!Error, %s" % repr(e))
 
 
-def db_add_device_record(db_path, device_info):
+def db_add_device_record(db_path, rec_name, rec_info):
     """Add a new or replace an existing record in the 'Devices' table."""
     path_exist = os.path.exists(db_path)
     if path_exist is False:
@@ -72,11 +87,8 @@ def db_add_device_record(db_path, device_info):
             # The REPLACE statement is an alias for the "INSERT OR REPLACE"
             # variant of the INSERT statement. This alias is provided for
             # compatibility with other SQL database engines.
-            tup = (device_info['device_name'],
-                   device_info['os_type'],
-                   device_info['ip_address'],
-                   ", ".join(device_info['interfaces']))
-            sql = ("REPLACE INTO Devices VALUES (?,?,?,?)")
+            tup = (rec_name, rec_info)
+            sql = ("REPLACE INTO Devices VALUES (?,?)")
             cursor.execute(sql, tup)
             conn.commit()
     except (db.OperationalError) as e:
@@ -96,15 +108,15 @@ def db_get_device_info(db_path, rec_name):
         with db.connect(db_path) as conn:
             cursor = conn.cursor()
             # Prepare and execute SQL statement
-            sql = ("SELECT * FROM Devices WHERE name=?")
+            sql = ("SELECT info FROM Devices WHERE name=?")
             cursor.execute(sql, (rec_name,))
             rec = cursor.fetchone()
-            return rec
+            return rec[0]
     except (db.OperationalError) as e:
         print("!!!Error, %s" % repr(e))
 
 
-def db_delete_device_record(db_path, record_name):
+def db_delete_device_record(db_path, rec_name):
     """Delete record matching to the given 'rec_name' from
     the 'Devices' table.
     """
@@ -116,11 +128,10 @@ def db_delete_device_record(db_path, record_name):
     try:
         with db.connect(db_path) as conn:
             cursor = conn.cursor()
-            # Prepare SQL query statement (make sure the 'record_name'
+            # Prepare and execute SQL statement (make sure the 'record_name'
             # parameter follows with comma to make it a tuple)
             sql = "DELETE FROM Devices WHERE name=?"
-            # Execute SQL query statement
-            cursor.execute(sql, (record_name,))
+            cursor.execute(sql, (rec_name,))
     except (db.OperationalError) as e:
         print("!!!Error, %s" % repr(e))
 
@@ -144,57 +155,57 @@ def db_print_table_rows_cnt(db_path, table_name):
     except (db.OperationalError) as e:
         print("!!!Error, %s" % repr(e))
 
+
 if __name__ == "__main__":
 
-    db_path = "./example1.db"
-
-    device_info1 = {'device_name': 'Router1',
-                    'os_type': 'IOS-XR',
-                    'ip_address': '10.30.30.1',
-                    'interfaces': ['Lo0', 'Mg0/0/CPU0/0',
-                                   'Gi0/0/0/0', 'Gi0/0/0/1',
-                                   'Gi0/0/0/2', 'Gi0/0/0/3']}
-    device_info2 = {'device_name': 'Router2',
-                    'os_type': 'IOS-XR',
-                    'ip_address': '10.30.30.2',
-                    'interfaces': ['Lo0', 'Mg0/0/CPU0/0',
-                                   'Gi0/0/0/0', 'Gi0/0/0/1',
-                                   'Gi0/0/0/2', 'Gi0/0/0/3']}
-    device_info3 = {'device_name': 'Router3',
-                    'os_type': 'IOS-XR',
-                    'ip_address': '10.30.30.3',
-                    'interfaces': ['Lo0', 'Mg0/0/CPU0/0',
-                                   'Gi0/0/0/0', 'Gi0/0/0/1',
-                                   'Gi0/0/0/2', 'Gi0/0/0/3']}
-    devices_info = [device_info1, device_info2, device_info3]
+    db_path = "./example2.db"
+    device1 = Device(name='Router1', os_type='IOS-XR',
+                     ip_addr='10.30.30.1',
+                     iflist=['Lo0', 'Mg0/0/CPU0/0',
+                             'Gi0/0/0/0', 'Gi0/0/0/1',
+                             'Gi0/0/0/2', 'Gi0/0/0/3'])
+    device2 = Device(name='Router2', os_type='IOS-XR',
+                     ip_addr='10.30.30.2',
+                     iflist=['Lo0', 'Mg0/0/CPU0/0',
+                             'Gi0/0/0/0', 'Gi0/0/0/1',
+                             'Gi0/0/0/2', 'Gi0/0/0/3'])
+    device3 = Device(name='Router3', os_type='IOS-XR',
+                     ip_addr='10.30.30.3',
+                     iflist=['Lo0', 'Mg0/0/CPU0/0',
+                             'Gi0/0/0/0', 'Gi0/0/0/1',
+                             'Gi0/0/0/2', 'Gi0/0/0/3'])
+    devices = [device1, device2, device3]
 
     print("\n").strip()
     print("Creating 'Devices' Table")
     print("\n").strip()
-    db_create_device_table(db_path)
+    db_create_devices_table(db_path)
     db_print_table_rows_cnt(db_path, 'Devices')
     print("\n").strip()
 
     print("Adding records to the 'Devices' Table")
     print("\n").strip()
-    for item in devices_info:
-        db_add_device_record(db_path, item)
+    for item in devices:
+        db_add_device_record(db_path, rec_name=item.name,
+                             rec_info=item.toJSON())
     db_print_table_rows_cnt(db_path, 'Devices')
     print("\n").strip()
 
     print("Searching for the records in the 'Devices' Table")
     print("\n").strip()
-    for item in devices_info:
-        rec_name = item['device_name']
-        device_info = db_get_device_info(db_path, rec_name)
+    for item in devices:
+        rec_name = item.name
+        js = db_get_device_info(db_path, rec_name)
+        d = json.loads(js)
+        device = Device(**d)
         print("%s" % ('<' * 78))
-        print_device_info(device_info)
+        print_device_info(device)
         print("%s" % ('>' * 78))
     print("\n").strip()
 
     print("Removing records from the 'Devices' Table")
     print("\n").strip()
-    for item in devices_info:
-        db_delete_device_record(db_path, item['device_name'])
+    for item in devices:
+        db_delete_device_record(db_path, item.name)
     db_print_table_rows_cnt(db_path, 'Devices')
     print("\n").strip()
